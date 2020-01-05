4d868498170b4fa0f05d813400c9cecba3f046c8
Lee Martin <leefredmart@gmail.com>
2020-01-05 14:27:10 -0500

    Added package.json's for all projects

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -1,4 +1,4 @@
-import html from '../images/HTML5_Logo_256.png' 
+import html from '../images/HTML5_Logo_256.png'
 import css from '../images/css3.png'
 import js from '../images/js-small.gif'
 import react from '../images/react.png'
@@ -11,10 +11,16 @@ import goatRelic3 from '../images/goatrelic3.jpg'
 import goatRelic4 from '../images/goatrelic4.jpg'
 import goatRelic5 from '../images/goatrelic5.jpg'
 import goatRelic6 from '../images/goatrelic6.jpg'
+import goatRelic7 from '../images/goatrelic7.jpg'
+import goatRelic8 from '../images/goatrelic8.jpg'
+import goatRelic9 from '../images/goatrelic9.jpg'
+import goatRelic10 from '../images/goatrelic10.jpg'
+
 
 import mealsApp1 from '../images/mealsapp1.png'
 import mealsApp2 from '../images/mealsapp2.png'
 import mealsApp3 from '../images/mealsapp3.png'
+import mealsApp4 from '../images/mealsapp4.jpg'
 
 import reactSsr1 from '../images/reactssr.jpg'
 import reactSsr2 from '../images/reactssr2.jpg'
@@ -23,8 +29,14 @@ import reactSsr4 from '../images/reactssr4.jpg'
 import reactSsr5 from '../images/reactssr5.jpg'
 import reactSsr6 from '../images/reactssr6.jpg'
 import reactSsr7 from '../images/reactssr7.jpg'
+import reactSsr8 from '../images/reactssr8.jpg'
+import reactSsr9 from '../images/reactssr9.jpg'
+import reactSsr10 from '../images/reactssr10.jpg'
+
 
 import todoApp1 from '../images/todoApp.jpg'
+import todoApp2 from '../images/todoApp2.jpg'
+import todoApp3 from '../images/todoApp3.jpg'
 
 import underConstruction from '../images/construction.png'
 
@@ -43,10 +55,14 @@ export const goatRelic = [
     goatRelic3,
     goatRelic4,
     goatRelic5,
-    goatRelic6
+    goatRelic6,
+    goatRelic7,
+    goatRelic8,
+    goatRelic9,
+    goatRelic10
 ]
 
-export const mealsApp = [mealsApp1, mealsApp2, mealsApp3]
+export const mealsApp = [mealsApp1, mealsApp2, mealsApp3, mealsApp4]
 
 export const reactSsr = [
     reactSsr1,
@@ -55,9 +71,12 @@ export const reactSsr = [
     reactSsr4,
     reactSsr5,
     reactSsr6,
-    reactSsr7
+    reactSsr7,
+    reactSsr8,
+    reactSsr9,
+    reactSsr10
 ]
 
-export const todoApp = [todoApp1]
+export const todoApp = [todoApp1, todoApp2, todoApp3]
 
 export const shopApp = [underConstruction]
