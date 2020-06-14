1562f7d8ca7b8968bd842947080a6992138bf804
Lee Martin <leefredmart@gmail.com>
2020-06-14 19:59:08 -0400

    Added first blog post

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import { BLOGS } from '../data/blogs'
+
 const Blog = () => (
     <div className='container'>
-        <div className='sentence'>Blog</div>
+        <div className='blog-title'>Blog</div>
+        {BLOGS.map((blog, index) => (
+            <div className='day-blog' key={index}>
+                <h1>{`Day ${index + 1}`}</h1>
+                <p>{blog}</p>
+            </div>
+        ))}
     </div>
 )
 
